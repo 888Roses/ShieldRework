fcbfd70a3ee2a28559f30aefa1c75b99703a9745
rosenoire <162016745+888Roses@users.noreply.github.com>
2026-05-27 18:02:29 +0200

    Fixed issue with invokevirtual propertected access blabla

--- src/main/java/dev/rosenoire/shieldrework/mixin/PlayerEntityMixin.java
+++ src/main/java/dev/rosenoire/shieldrework/mixin/PlayerEntityMixin.java
@@ -1,18 +1,19 @@
 package dev.rosenoire.shieldrework.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.BlocksAttacks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public class PlayerEntityMixin {
-    @Inject(method = "blockUsingItem", at = @At("HEAD"), cancellable = true)
-    public void shield_rework$takeShieldHit(ServerLevel level, LivingEntity attacker, CallbackInfo ci) {
-        attacker.blockedByItem((Player) (Object) this);
-        ci.cancel();
+    @WrapOperation(method = "blockUsingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/component/BlocksAttacks;disable(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/entity/LivingEntity;FLnet/minecraft/world/item/ItemStack;)V"))
+    public void blockUsingItem$dontDisableShield(BlocksAttacks instance, ServerLevel level, LivingEntity user, float baseSeconds, ItemStack blockingWith, Operation<Void> original) {
     }
 }
